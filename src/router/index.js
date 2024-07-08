6152fc0aac07c014b42a664da4979567bd46695a
SungYoun <32204078@daou.co.kr>
2024-07-08 16:22:41 +0900

    fix: 로그인 기능 구현 중

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,7 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes // 기본 라우트 설정을 사용합니다.
+  routes // 기본 라우트 설정을 사용.
 })
 
 export default router
