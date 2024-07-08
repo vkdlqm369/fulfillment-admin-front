d5535ff5cec3f5042424aa542674a5b1b8f21349
염수인 <vkdlqm369@naver.com>
2024-07-08 13:23:55 +0900

    fix: response body 변경, show_list 변경 시 1 페이지로 변경

--- src/components/SearchBtn.vue
+++ src/components/SearchBtn.vue
@@ -1,11 +1,3 @@
-<script setup>
-import { useInputTextForSearchStore } from "@/stores/index";
-
-const store = useInputTextForSearchStore()
-
-</script>
-
-
 <template>
   <v-btn style="width:80px; height:50px;">
     <v-icon>mdi-account-search</v-icon>
