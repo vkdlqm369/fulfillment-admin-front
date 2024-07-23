e0c45b1fb642b2ad7fa99cf3eeeb38ef8939f3cc
홍진수 <h1101201@daou.co.kr>
2024-07-23 14:58:41 +0900

    fix: 디렉토리 경로수정

---
+++ src/components/ordercollectpopup/PopupCloseButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="popup-close-button">
+    <button @click="closePopup">닫기</button>
+  </div>
+</template>
+
+<script setup>
+
+function closePopup() {
+  window.close();
+}
+</script>
+
+<style scoped>
+.popup-close-button {
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
